11a6a5f52f509a4e9aba6015df704fe2379ec7b7
Simon Baslé <simon.basle@gmail.com>
2014-11-13 12:42:44 +0100

    add methods to create entry document with ttl
    convert values to Serializable (and only accept Serializable), determine
    correct ttl from policy and type of operation, prepare a Serializable
    Document.
    
    we will have to find a better way of enforcing Serializable values.

--- src/main/java/com/couchbase/client/jcache/CouchbaseCache.java
+++ src/main/java/com/couchbase/client/jcache/CouchbaseCache.java
@@ -23,6 +23,7 @@ package com.couchbase.client.jcache;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
@@ -33,6 +34,7 @@ import javax.cache.Cache;
 import javax.cache.CacheManager;
 import javax.cache.configuration.CacheEntryListenerConfiguration;
 import javax.cache.configuration.Configuration;
+import javax.cache.expiry.Duration;
 import javax.cache.expiry.ExpiryPolicy;
 import javax.cache.integration.CacheLoader;
 import javax.cache.integration.CacheWriter;
@@ -44,6 +46,7 @@ import javax.cache.processor.EntryProcessorResult;
 import com.couchbase.client.core.logging.CouchbaseLogger;
 import com.couchbase.client.core.logging.CouchbaseLoggerFactory;
 import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.document.SerializableDocument;
 import com.couchbase.client.jcache.management.CouchbaseCacheMxBean;
 import com.couchbase.client.jcache.management.CouchbaseStatisticsMxBean;
 import com.couchbase.client.jcache.management.ManagementUtil;
@@ -57,6 +60,12 @@ import com.couchbase.client.jcache.management.ManagementUtil;
 public class CouchbaseCache<K, V> implements Cache<K, V> {
 
     private static final CouchbaseLogger LOGGER = CouchbaseLoggerFactory.getInstance(CouchbaseCache.class);
+
+    /** 30 days in seconds */
+    private static final long MAX_TTL = 30 * 24 * 60 * 60;
+    private static final int TTL_DONT_CHANGE = -2;
+    private static final int TTL_EXPIRED = -1;
+    private static final int TTL_NONE = 0;
 
     private final CouchbaseCacheManager cacheManager;
     private final String name;
@@ -109,13 +118,6 @@ public class CouchbaseCache<K, V> implements Cache<K, V> {
         this.keyPrefix = configuration.getCachePrefix();
         this.bucket = cacheManager.getCluster().openBucket(configuration.getBucketName(),
                 configuration.getBucketPassword());
-    }
-
-    protected String toStringKey(K key) {
-        if (key == null) {
-            throw new NullPointerException("Keys must not be null");
-        }
-        return keyPrefix + String.valueOf(key);
     }
 
     /**
@@ -409,5 +411,95 @@ public class CouchbaseCache<K, V> implements Cache<K, V> {
         if (isClosed()) {
             throw new IllegalStateException("Cache " + name + " closed");
         }
+    }
+
+    private int getDurationCode(Operation op) {
+        ExpiryPolicy policy = configuration.getExpiryPolicyFactory().create();
+        Duration expiryDuration = Duration.ZERO;
+        switch (op) {
+            case CREATION:
+                expiryDuration = policy.getExpiryForCreation();
+                break;
+            case UPDATE:
+                expiryDuration = policy.getExpiryForUpdate();
+                break;
+            case ACCESS:
+                expiryDuration = policy.getExpiryForAccess();
+                break;
+        }
+
+        if (expiryDuration == null) {
+            return TTL_DONT_CHANGE;
+        } else if (expiryDuration.isZero()) {
+            return TTL_EXPIRED;
+        } else if (expiryDuration.isEternal()) {
+            return TTL_NONE;
+        } else {
+            long longExpiry = expiryDuration.getTimeUnit().toSeconds(expiryDuration.getDurationAmount());
+            if (longExpiry <= MAX_TTL) {
+                return (int) longExpiry;
+            } else {
+                throw new IllegalArgumentException("Explicit " + op.name()
+                        + " expiry must be less than 30 days (30 * 24 * 60 * 60 = " + MAX_TTL + "sec)");
+            }
+        }
+    }
+
+    private void touchIfNeeded(String docId) {
+        int ttlOrCode = getDurationCode(Operation.ACCESS);
+        if (ttlOrCode >= 0) {
+            bucket.touch(docId, ttlOrCode);
+        }
+    }
+
+    /**
+     * Depending on the operation, produces a SerializableDocument with correct TTL.
+     *
+     * @param key the key for the document
+     * @param value the value to store
+     * @param op the operation being performed
+     * @return the {@link SerializableDocument} to be persisted
+     * @throws IllegalArgumentException when the {@link ExpiryPolicy} produces a TTL > 30 days
+     * @throws IllegalStateException when the {@link ExpiryPolicy} indicates a TTL already expired
+     */
+    private SerializableDocument createDocument(K key, V value, Operation op) {
+        String cbKey = toInternalKey(key);
+        Serializable cbValue = toInternalValue(value);
+        int ttlOrCode = getDurationCode(op);
+        switch (ttlOrCode) {
+            case TTL_DONT_CHANGE:
+                return SerializableDocument.create(cbKey, cbValue);
+            case TTL_NONE:
+                return SerializableDocument.create(cbKey, cbValue);
+            case TTL_EXPIRED:
+                throw new IllegalStateException("Policy indicates expiration for " + op.name());
+            default:
+                if (ttlOrCode < 0) {
+                    throw new IllegalArgumentException("Unknown ttl code " + ttlOrCode);
+                } else {
+                    return SerializableDocument.create(cbKey, ttlOrCode, cbValue);
+                }
+        }
+    }
+
+    protected String toInternalKey(K key) {
+        if (key == null) {
+            throw new NullPointerException("Keys must not be null");
+        }
+        return keyPrefix + String.valueOf(key);
+    }
+
+    private Serializable toInternalValue(V value) {
+        if (value instanceof Serializable) {
+            return (Serializable) value;
+        } else {
+            throw new ClassCastException("This cache can only accept Serializable values");
+        }
+    }
+
+    private static enum Operation {
+        CREATION,
+        UPDATE,
+        ACCESS
     }
 }
